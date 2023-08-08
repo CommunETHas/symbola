c05c81ebeed3f842f2db6d1d33c7fc7181ddc7b9
acroquelois <croquelois.adrien@gmail.com>
2023-08-08 13:56:24 +0200

    feat(routing): add routing

---
+++ src/pages/Events.tsx
@@ -0,0 +1,7 @@
+export function Events() {
+    return (
+        <>
+            <h1>Events</h1>
+        </>
+    );
+}
---
+++ src/router/router.tsx
@@ -0,0 +1,53 @@
+import React, { StrictMode } from "react";
+import {
+  Outlet,
+  RouterProvider,
+  Link,
+  Router,
+  Route,
+  RootRoute,
+} from "@tanstack/router";
+import { Events } from "../pages/Events";
+import { CreateTicketing } from "../pages/CreateTicketing";
+import { Home } from "../pages/Home";
+import { Navbar } from "../layout/Navbar";
+
+// Create a root route
+const rootRoute = new RootRoute({
+  component: Navbar,
+});
+
+const homeRoute = new Route({
+  getParentRoute: () => rootRoute,
+  path: "/",
+  component: Home,
+});
+
+const eventsRoute = new Route({
+  getParentRoute: () => rootRoute,
+  path: "/events",
+  component: Events,
+});
+
+const createTicketingRoute = new Route({
+  getParentRoute: () => rootRoute,
+  path: "/create-ticketing",
+  component: CreateTicketing,
+});
+
+// Create the route tree using your routes
+const routeTree = rootRoute.addChildren([
+  homeRoute,
+  eventsRoute,
+  createTicketingRoute,
+]);
+
+// Create the router using your route tree
+export const router = new Router({ routeTree });
+
+// Register your router for maximum type safety
+declare module "@tanstack/router" {
+  interface Register {
+    router: typeof router;
+  }
+}
